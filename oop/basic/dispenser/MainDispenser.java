8a5520ccbae0aab815d5cf98bc8e165020ab76ed
Vitalii Vinnyk <vinvit82@gmail.com>
2023-01-13 23:27:57 +0200

    upd

--- oop/basic/dispenser/MainDispenser.java
+++ oop/basic/dispenser/MainDispenser.java
@@ -1,7 +1,10 @@
-package oop.basic.storage;
+package oop.basic.dispenser;
+
+import oop.basic.dispenser.DispenserMethods;
 
 public class MainDispenser {
     public static void main(String[] args) {
-        new DispenserMethods();
+        DispenserMethods product = new DispenserMethods();
+        product.buyProduct();
     }
 }
