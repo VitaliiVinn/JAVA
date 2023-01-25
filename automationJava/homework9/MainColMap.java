8b3580270e7359914d5339eb3812c729c9fb3a8b
Vitalii Vinnyk <vinvit82@gmail.com>
2023-01-25 22:55:07 +0200

    ht9&10

---
+++ automationJava/homework9/MainColMap.java
@@ -0,0 +1,8 @@
+package automationJava.homework9;
+
+public class MainColMap {
+    public static void main(String[] args) {
+        ColMap colMap = new ColMap();
+        ColMap.printMap();
+    }
+}
