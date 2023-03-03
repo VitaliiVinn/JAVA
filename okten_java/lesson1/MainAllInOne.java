ddd439eceb193d077690eb9a3308e5d294cfc50d
Vitalii Vinnyk <vinvit82@gmail.com>
2023-03-03 10:58:14 +0200

    posts

--- okten_java/lesson1/MainAllInOne.java
+++ okten_java/lesson1/MainAllInOne.java
@@ -21,8 +21,17 @@ public class MainAllInOne {
 //                + book.title + "\nAmount of pages are " + book.pages + "\nGenre is " + book.genre + "\n}");
         System.out.println(gson.toJson(book));
 
-
         Dog dog = new Dog("Tuzik", 45, "Rezenshnautzer");
         System.out.println(gson.toJson(dog));
+
+        Post post = new Post(1, 1, "sunt aut facere repellat provident occaecati excepturi optio " +
+                "reprehenderit", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit " +
+                "molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
+        System.out.println(post);
+
+        Comment comment = new Comment(1, 1, "id labore ex et quam laborum", "Eliseo@gardner.biz",
+                "laudantium enim quasi est quidem magnam voluptate ipsam eos\nntempora quo necessitatibus\\" +
+                        "ndolor quam autem quasi\nnreiciendis et nam sapiente accusantium");
+        System.out.println(comment);
     }
 }
